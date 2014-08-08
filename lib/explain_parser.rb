91dbad530ce423af48b76987894a237ebc9802cd
ainame <satoshi.namai@mixi.co.jp>
2014-08-08 15:55:05 +0000

    support Mysql2::Result

--- lib/explain_parser.rb
+++ lib/explain_parser.rb
@@ -43,7 +43,6 @@ class ExplainParser
   end
 
   def initialize(explain)
-    raise ArgumentError unless explain
     @explain = explain
   end
 
@@ -52,36 +51,78 @@ class ExplainParser
   end
 
   def call
-    return unless rows && !rows.empty?
-    build
-  end
-
-  def lines
-    @explain.each_line.to_a
-  end
-
-  def rows
-    @rows ||= lines.select{|line| line =~ /\w+/ && line !~ /\d+ row in set/ }
-  end
-
-  def keys()
-    @keys ||= rows[0].chomp.split('|').compact.map(&:strip).reject(&:empty?).map(&:downcase)
-  end
-
-  def values_list()
-    rows[1..-1].map{|row| cleanup_values(row.chomp.split('|').compact) }
-  end
-
-  def cleanup_values(dirty_values)
-    dirty_values.map(&:strip).reject(&:empty?).map {|val| val == 'NULL' ? nil : val }
-  end
-
-  def build
-    values_list.reduce([]) do |explains, values|
-      params = keys.zip(values).inject({}) { |h, (k, v)| h[k] = v; h }
-      explains << ExplainParser::Explain.new(params)
-      explains
+    case @explain
+    when ::String
+      FromTableString.new(@explain).call
+    when ::Mysql2::Result
+      FromMysql2Result.new(@explain).call
     end
   end
 
+  class Base
+    def self.call(explain)
+      new(explain).call
+    end
+
+    def call
+      build
+    end
+
+    def keys
+      'override me'
+    end
+
+    def values_list
+      'override me'
+    end
+
+    def cleanup_values(dirty_values)
+      dirty_values.map(&:strip).reject(&:empty?).map {|val| val == 'NULL' ? nil : val }
+    end
+
+    def build
+      values_list.reduce([]) do |explains, values|
+        params = keys.zip(cleanup_values(values)).inject({}) { |h, (k, v)| h[k] = v; h }
+        explains << ExplainParser::Explain.new(params)
+        explains
+      end
+    end
+  end
+
+  class FromMysql2Result < Base
+    def initialize(explain)
+      @explain = explain
+    end
+
+    def keys
+      @explain.fields
+    end
+
+    def values_list
+      @exlpain.to_a
+    end
+  end
+
+  class FromTableString < Base
+    def initialize(explain)
+      raise ArgumentError unless explain
+      @explain = explain
+    end
+
+    def lines
+      @explain.each_line.to_a
+    end
+
+    def rows
+      @rows ||= lines.select{|line| line =~ /\w+/ && line !~ /\d+ row in set/ }
+    end
+
+    def keys()
+      @keys ||= rows[0].chomp.split('|').compact.map(&:strip).reject(&:empty?).map(&:downcase)
+    end
+
+    def values_list()
+      rows[1..-1].map{|row| row.chomp.split('|').compact }
+    end
+  end
 end
